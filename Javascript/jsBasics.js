98d9a40cb9e0760df4914beab714129bd6076f79
ShivaKumar7799 <shivakumarbammidi@gmail.com>
2022-09-11 13:05:15 +0530

    uploading js basic concepts

---
+++ Javascript/jsBasics.js
@@ -0,0 +1,221 @@
+// console.log("my next js");
+// console.log("my second js");
+// console.log("my third js");
+// console.log("my fourth js")
+
+// document.getElementById("display").innerHTML = "user logged in"
+
+// document.write("display in write");
+
+// window.alert("alert displayed here");
+// let firstname = window.prompt("Enter your name");
+// console.log("your name is",firstname)
+// document.getElementById('display').innerHTML =  firstname + " logged in";
+// window.print()
+
+// let x;
+// let y;
+// let z;
+// z = x + y;
+// x = 10;
+// y= 20;
+// z = x + y;
+
+// console.log(z)
+// document.getElementById("display").innerHTML = "the value of z =" + z;
+// document.write("the value of z =" + z)
+
+// let firstName = "shiva";
+// let lastName = "kumar";
+
+// document.getElementById("display").innerHTML = "your name is " + firstName + " " + lastName;
+
+// let x= 10; y = 20; 
+
+// let add = x + y;
+// let sub =  x - y;
+// let mul = x * y;
+// let division = x / y;
+// let remainder = x % y;
+// let sqr = x ** y;
+
+// console.log("add = ",add,"sub =",sub,"mul =",mul, "division =", division, "remainder =" + remainder + "square =" + sqr ) 
+// document.getElementById("display").innerHTML = "add = " + add + "sub =" + sub  ;
+
+// let x = 2 ; let y =3;
+// let sum = x + y;
+
+// variable names 
+//dollar symbol, underscore, capital or small letter
+//not valid for variable names start with number
+// let _wishes = "hello"
+// let Wishes = "hello world"
+// console.log(_wishes,Wishes)
+
+//camelCase, PascalCase, snake case
+
+// let camelCase = "This is camel case";
+// let PascalCase = "This is pascal case"
+// let snake_case = "This is snake case"
+
+// console.log(firstName)
+// var firstName;
+
+// var firstName = "shiva";
+// var firstName = "kumar"
+// console.log(firstName)
+
+// let firstName = "shiva";
+//  firstName = firstName + "kumar"
+// console.log(firstName)
+
+// var x = 10;
+// console.log(x)
+// {
+//   console.log(x)
+//   var x = 2;
+//   console.log(x)
+// }
+// console.log(x)
+// var x = 20;
+// console.log(x)
+
+// let x = 10;
+// console.log(x)
+// {
+//   // console.log(x)
+//   let x = 2;
+//   console.log(x);
+//   x = 4;
+//   console.log(x)
+// }
+// console.log(x)
+// x = 20;
+// console.log(x)
+
+// const 
+
+// const x = 5;   variables cannot be reassigned
+    
+// Hoisting
+
+// console.log(x);
+// var x = 5;
+// console.log(x);
+
+// console.log(x);
+// let x = 5;
+// console.log(x);
+
+// Assignment Operators
+
+// let x = 5;
+//     x = x + 10; 
+//     x += 10;   // x = x + 10
+//     x *= 20;  // x = x * 20
+// console.log(x)
+
+// type of operator
+
+// console.log(typeof 5);
+// console.log(typeof "shiva");
+// console.log(typeof false);
+
+// var a = 5;
+// var b = "shiva";
+// var c = true;
+// var d = ["shiva","kumar"];
+// var e = {
+//   firstName : "shiva",
+//   lastName : "kumar"
+// }
+// var f = () => {
+//   return "hello"
+// }
+
+// console.log(a, typeof a);
+// console.log(b, typeof b);
+// console.log(c, typeof c);
+// console.log(d, typeof d, Array.isArray(d));
+// console.log(e, typeof e, Array.isArray(e));
+// console.log(f, typeof f);
+
+// storing values in array
+
+// let fruits = ["apple", "banana", 4 , 7 , true, false, ["first fruit", "second fruit"], { firstName : "shiva", lastName : "kumar", f }, f]
+
+// console.log(fruits[2])
+
+// let name = "shiva"
+// console.log(name.length)
+
+// Escape characters
+
+// let someText = 'It\'s already \"john\" gone';
+// console.log(someText);
+
+//strict equality operator
+
+// let five = "5";
+// let numberFive = 5;
+// console.log(five === numberFive);
+
+//slice operator
+// let firstName = "shivakumar and shivakumar";
+// console.log(firstName.slice(0,3))  // 0 and 5 are starting and ending indexes(upto)
+
+// replace
+// console.log(firstName.replace("shiva","madhu"))  // only matches the first word in the string
+
+// uppercase and lower case
+// console.log(firstName.toUpperCase())
+// console.log(firstName.toLowerCase());
+
+// string concatenation
+
+// let firstName = "shiva";
+// let lastName = "kumar";
+// console.log(firstName + " " + lastName)
+
+// trim method
+// let enterName = window.prompt("enter your name")
+// console.log(enterName.length);
+// console.log(enterName.trim().length);
+// console.log(enterName == enterName.trim())
+
+// let wishes = "hello";
+// console.log(wishes.charAt(1))
+
+//split method converts string to array
+// let fruits= "apple,banana,orange";
+// console.log(fruits)
+// console.log(fruits.split(","))
+// console.log(fruits.slice(6,12))
+
+// includes
+
+// console.log(fruits.includes("pine apple"))
+
+// string literals
+
+// let favouriteFruite = "apple";
+// console.log("my favourite fruit is" + " " + favouriteFruite);
+// console.log(`my favourite fruit is ${favouriteFruite}`)
+
+// NaN not a number
+
+// let x= 10;
+// console.log( x / "apple");
+
+// numbers
+// let x = 123;
+// console.log(x,typeof x)
+// let numberToString = x.toString();
+// console.log(numberToString, typeof numberToString);
+
+// let x = 9.4576;
+// console.log(x.toFixed(3))
+// console.log(Number(x), typeof Number(x));
+// console.log(parseInt(x));
+// console.log(parseFloat(x))
+
